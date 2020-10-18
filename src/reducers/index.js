22f16c861e58f6ef61ae23fad2107fba0f7cf908
SpaYco <zizou.aziz101@gmail.com>
2020-10-18 11:28:39 +0100

    added reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import recipesRecuder from './recipes';
+
+const rootReducer = combineReducers({
+  recipes: recipesRecuder,
+});
+
+export default rootReducer;
---
+++ src/reducers/recipes.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {};
+
+const recipesReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default recipesReducer;
