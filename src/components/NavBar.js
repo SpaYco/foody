e532363294c21a55e6e9d9e151aa3760b86cd6df
SpaYco <zizou.aziz101@gmail.com>
2020-10-20 04:08:16 +0100

    updated details UI

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const NavBar = ({ updateIndex, updateSearch }) => (
-  <nav>
-    <p onClick={updateIndex('home')} aria-hidden>Foody</p>
-    <SearchBar updateIndex={updateIndex} updateSearch={updateSearch} />
-    <a href="https://github.com/Spayco" target="_blank" rel="noopener noreferrer">By SpaYco</a>
-  </nav>
-);
+const NavBar = ({ updateIndex, updateSearch }) => {
+  const handleClick = () => {
+    updateIndex('home');
+  };
+  return (
+    <nav>
+      <p onClick={handleClick} aria-hidden>Foody</p>
+      <SearchBar updateIndex={updateIndex} updateSearch={updateSearch} />
+      <a href="https://github.com/Spayco" target="_blank" rel="noopener noreferrer">By SpaYco</a>
+    </nav>
+  );
+};
 
 export default NavBar;
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -27,32 +27,35 @@ class RecipeDetails extends React.Component {
     const { data } = this.props;
     const { details } = this.state;
     return (
-      <div className="details-section">
-        <div className="details-tab">
+      <div id="details-section">
+        <div id="details-tab">
           <img src={data.image} alt={data.title} />
           <p>{data.title}</p>
         </div>
-        <div>
+        <div id="details">
           <div className="types">
             <h1>Types:</h1>
-            <div className={this.classType(details.vegetarian)}>vegetarian</div>
-            <div className={this.classType(details.vegan)}>vegan</div>
-            <div className={this.classType(details.glutenFree)}>gluten Free</div>
-            <div className={this.classType(details.dairyFree)}>dairy Free</div>
-            <div className={this.classType(details.veryHealthy)}>very Healthy</div>
+            <div className="detail-btns">
+              <div className={this.classType(details.vegetarian)}>vegetarian</div>
+              <div className={this.classType(details.vegan)}>vegan</div>
+              <div className={this.classType(details.glutenFree)}>gluten Free</div>
+              <div className={this.classType(details.dairyFree)}>dairy Free</div>
+              <div className={this.classType(details.veryHealthy)}>very Healthy</div>
+            </div>
           </div>
           <div className="kinds">
             <h1>Kinds:</h1>
-            <div className={this.classType(details.cheap)}>cheap</div>
-            <div className={this.classType(details.veryPopular)}>very Popular</div>
-            <div className={this.classType(details.sustainable)}>sustainable</div>
+            <div className="detail-btns">
+              <div className={this.classType(details.cheap)}>cheap</div>
+              <div className={this.classType(details.veryPopular)}>very Popular</div>
+              <div className={this.classType(details.sustainable)}>sustainable</div>
+            </div>
           </div>
-          <div className="summary">
+          <div id="summary">
             <h1>Summary:</h1>
             <Interweave
               content={details.summary}
             />
-            ;
           </div>
         </div>
       </div>
